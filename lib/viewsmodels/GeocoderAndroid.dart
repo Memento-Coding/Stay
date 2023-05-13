3f9945cc52334926fbd61363c40e0205588f4a34
Juan Hoyos <juanhoyos1347@gmail.com>
2023-05-13 01:35:20 -0500

    eventos, sitios y ciudad

---
+++ lib/viewsmodels/GeocoderAndroid.dart
@@ -0,0 +1,15 @@
+import 'package:geolocator/geolocator.dart';
+
+class GeoCoderAndroid {
+  Future<Position> determinePosicion() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('error');
+      }
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+}
